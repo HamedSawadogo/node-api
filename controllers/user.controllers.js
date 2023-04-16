36b38d0f0f4eb0b7cf600f85a3c840abcddc939f
Sawadogo Hamed <102871528+Sawadogoxxx@users.noreply.github.com>
2023-04-16 21:40:45 +0100

    exo1

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,11 +1,22 @@
 const User = require("../model/user.model");
+const bcrypt = require("bcrypt");
 
 module.exports.addUser = async (req, res) => {
-  const user = new User({ ...req.body });
-  user
-    .save()
-    .then(() => res.status(201).json({ message: "objet crée avec success!" }))
-    .catch((error) => res.status(400).json({ error }));
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        ...req.body,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() =>
+          res.status(201).json({ message: "objet crée avec success!" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 /**
@@ -17,40 +28,30 @@ module.exports.usersData = async (req, res) => {
     .catch((error) => res.status(400).json({ error }));
 };
 /**
- * ajouter un utilisateur a */
-// /**
-//  *
-//  * supprimer un utilisateur
-//  */
-// module.exports.deleteUser = async (req, res) => {
-//   if (!req.params.id) {
-//     return;
-//   }
-//   res.status(201).json({ message: "suppresion effectuté avec success!" });
-//   // try {
-//   //   // console.log(req.params.id);
-//   //   // const user = await User.findById(req.params.id);
-//   //   // await user.remove();
-
-//   // } catch (error) {
-//   //   res.status(400).json({ error });
-//   // }
-// };
-// /**
-//  * rechercher un utilisateur
-//  * @param {*} req
-//  * @param {*} res
-//  */
-// module.exports.findUser = async (req, res) => {
-//   const user = await User.findById(req.params.id);
-//   user
-//     .then((res) =>
-//       res.status(200).json({
-//         res,
-//       })
-//     )
-//     .catch((error) => res.status(400).json({ error }));
-// };
+ * supprimer un utilisateur
+ */
+module.exports.deleteUser = async (req, res) => {
+  if (!req.params.id) {
+    return;
+  }
+  try {
+    User.deleteOne({ _id: req.params.id })
+      .then(() =>
+        res.status(201).json({ message: "utilisateur supprimé avec success!" })
+      )
+      .catch((error) => res.status(400).json({ error }));
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+/**
+ * rechercher un utilisateur
+ */
+module.exports.findUser = async (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json({ thing }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 // module.exports.upDateUser = async (req, res) => {
 //   res.json({ response: "modification d'un utilisateur en Cours..." });
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { addUser, usersData } = require("../controllers/user.controllers");
+const {
+  addUser,
+  usersData,
+  deleteUser,
+  findUser,
+} = require("../controllers/user.controllers");
 const router = express.Router();
 
 router.get("/", usersData);
 router.post("/", addUser);
-// router.delete("/", deleteUser);
+router.delete("/:id", deleteUser);
 // router.put("/", upDateUser);
-// router.get("/:id", findUser);
+router.get("/:id", findUser);
 
 module.exports = router;
